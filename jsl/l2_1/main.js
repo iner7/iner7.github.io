fbcc7248eba3579e5b890bf96eec8a224acb3eb9
iner7 <37867876+iner7@users.noreply.github.com>
2022-05-12 09:58:05 +0300

    v

--- jsl/l2_1/main.js
+++ jsl/l2_1/main.js
@@ -2,8 +2,8 @@
 
 document.getElementById("infp").innerHTML = "";
 
-document.getElementById("equaler").addEventListener("click", function() {
+document.getElementById("equaler").onclick = function() {
     let x = parseFloat(document.getElementById("xxx").innerHTML);
     let y = parseFloat(document.getElementById("yyy").innerHTML);
     document.getElementById("ttt").innerHTML = pow(x, y);
-});
+};
