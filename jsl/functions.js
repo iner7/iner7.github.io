d71782eacd23bfa89148277fab74134317eb0e26
iner7 <37867876+iner7@users.noreply.github.com>
2022-05-12 10:07:21 +0300

    u

--- jsl/functions.js
+++ jsl/functions.js
@@ -10,3 +10,17 @@
 function pow(x, n) {
     return Math.pow(x, n);
 }
+
+/**
+ * для данного натурального n вычисляет сумму чисел от 1 до n. 
+ *
+ * @param {number} n натурального число.
+ * @return {number}  сумму чисел от 1 до n. 
+ */
+ function sumTo(n) {
+    let s = 1;
+    for (let i = 2; i <= 55; i++) {
+        s += i;
+    }
+    return s;
+}
