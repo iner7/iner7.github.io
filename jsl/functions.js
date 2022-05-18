0a6b1e632444fe85454f4e37e95d28e0c77daf55
iner7 <37867876+iner7@users.noreply.github.com>
2022-05-18 10:24:17 +0300

    JCJILDSALJKHSvbjk

--- jsl/functions.js
+++ jsl/functions.js
@@ -1,5 +1,4 @@
 'use strict'; 
-
 /**
  * Возвращает x, возведённое в n-ную степень.
  *
@@ -10,7 +9,6 @@
 function pow(x, n) {
     return Math.pow(x, n);
 }
-
 /**
  * для данного натурального n вычисляет сумму чисел от 1 до n. 
  *
@@ -24,7 +22,6 @@ function sumTo(n) {
     }
     return s;
 }
-
 /**
  *  возвращает факториал числа n!, используя рекурсивный вызов. 
  *
@@ -35,7 +32,6 @@ function factorial(n) {
     if (n == 0 || n == 1) return 1;
     return n * factorial(n - 1);
 }
-
 /**
  *  возвращает n-е число Фибоначчи.
  *
@@ -54,7 +50,6 @@ function fib(n) {
     }
     return b;
 }
-
 /**
  *  принимает целочисленное значение x и возвращает анонимную функцию.
  *  Анонимная функция должна принимать целочисленное значение y и
@@ -74,7 +69,6 @@ function compare(x) {
         else return null;
     };
 }
-
 /**
  *  и выводит числа от 0 до num в красивом японском виде в консоль
  *
@@ -83,18 +77,15 @@ function compare(x) {
  */
 function printNumbers(num, cols) {
     let r = Math.ceil(num / cols);
-
     for (let i = 0; i < r; i++) {
         let s = "";
         for (let j = 0; j < cols; j++) {
             let nm = j*r + i;
             if (nm < num) s += nm + "\t";
         }
-
         console.log(s);
     }
 }
-
 /**
  * ОПИСАНИЕ
  *
@@ -104,7 +95,6 @@ function printNumbers(num, cols) {
 function prima(num) {
     
 }
-
 /**
  *  возвращает дробную часть числа num. 
  *
@@ -113,20 +103,16 @@ function prima(num) {
  */
 function getDecimal(num) {
     return num - Math.floor(num);
-}/**
- * для натурального n возвращает массив, заполненный числами Фибоначчи до n-го (не включая его). 
+}
+/**
+ * которая возвращает строку str с заглавным первым символом. 
  *
- * @param {number} num число 
- * @return {Array}   массив чисел
+ * @param {string} str строка 
+ * @return {string}   строка
  */
-function fibs(num) {
-    let u = [];
-    for (let i = 0; i > n; i++) x.push(fib(i));
-    return u;
-}st(str) {
+function ucFirst(str) {
     return str[0].toLocaleUpperCase() + str.slice(1, str.length);
 }
-
 /**
  *  возвращает true, если строка str содержит 'viagra' или 'XXX', а иначе false. Функция должна быть нечувствительна к регистру. 
  *
@@ -136,7 +122,6 @@ function fibs(num) {
 function checkSpam(str) {
     return ( str.toLocaleLowerCase().search("(viagra|xxx)") != -1 );
 }
-
 /**
  * которая проверяет длину строки str, и если она превосходит maxlength – заменяет конец str на символ многоточие "…"
  *
@@ -148,7 +133,6 @@ function truncate(str, maxlength) {
     if (str.length <= maxlength) return str;
     else return str.slice(0, maxlength-1) + "\u2026"
 }
-
 /**
  * преобразуйте строку вида 'var-test-text' в 'varTestText'. 
  * То есть, дефисы удаляются, а все слова после них 
@@ -162,7 +146,6 @@ function camelize(str) {
         return index === 0 ? word.toLowerCase() : word.toUpperCase();
     }).replace(/(\s|!|-)+/g, '');
 }
-
 /**
  * для натурального n возвращает массив, заполненный числами Фибоначчи до n-го (не включая его). 
  *
